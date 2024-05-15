c97d2baae4b60af4f917fe6d5eb0a86f9f871539
MANNEZ4966 <bmstudio2551the@gmail.com>
2024-05-15 21:56:49 +0700

    update docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Mori Music Documention</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/MoriDev-2023',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/Qqj2kCd6Qd',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/MoriDev-2023',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2024 Mori Dev',
   },
 }
 
